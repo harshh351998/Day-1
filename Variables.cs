512df5771caff773276aa75c187343f919df38f6
harshh351998 <mendapara.harsh47@gmail.com>
2025-09-03 22:58:53 +0530

    #01:- Variables

---
+++ Variables.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 27;
+            string name = "Harsh";
+            bool isEmployed = true;
+            double height = 5.9;
+            Console.WriteLine($"age is {age} + name is {name} + employed: {isEmployed} + height: {height}");
+        }
+    }
+}
